6bf54cc465bfea4b74a722b31bf96f35a8d8a62f
J S NINAD <102415351+JSNinad@users.noreply.github.com>
2023-11-05 11:37:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://roltixsitenew-git-master-jsninad.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+   }
+ ))
 
 mongoose
   .connect(process.env.MONGODB_URI)
